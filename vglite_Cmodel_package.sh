a332f8396adf69280058afc4fc12f8e46853f016
IPD_SCM_CN <IPD_SCM_CN@verisilicon.com>
2023-12-04 18:53:03 +0800

    [CL778211] update vglite_Cmodel_package.sh

--- vglite_Cmodel_package.sh
+++ vglite_Cmodel_package.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
 # Example: 
-#          cd SW/VGLite/Hubi.dev
-#          ./release/vglite_Cmodel_package.sh  gc265  0x425
+#          cd SW/VGLite/Hubi.dev/release
+#          ./vglite_Cmodel_package.sh  gc265  0x425  Vivante_VGLite_Bin_Cmodel_0x425_4.0.51  Vivante_VGLite_Src_drv_gc265_0x425_4.0.51
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 4 ]; then
     echo
-    echo "Usage example:  $0  gc265  0x425"
+    echo "Usage example:  $0  gc265  0x425 Vivante_VGLite_Bin_Cmodel_0x425_4.0.51 Vivante_VGLite_Src_drv_gc265_0x425_4.0.51"
     echo
     exit 1
 fi
 
 ### Copy the chip specific header file
 
-cp -f VGLite/Series/$1/$2/vg_lite_options.h  VGLite/
+cp -f ../VGLite/Series/$1/$2/vg_lite_options.h  ../VGLite/
 
 
 ### Create release package
 
-rm -rf package
-mkdir package
+rm -rf ./../../../../$4
+mkdir  ./../../../../$4
+mkdir  ./../../../../$4/BUILD_vgHuBi
 
-cp -f  release/*        ./package
-cp -f  Makefile*        ./package
-cp -rf inc/             ./package
-cp -rf VGLite/          ./package
-cp -rf VGLiteKernel/    ./package
-cp -rf VGLite.def       ./package
+cp -f  ./*                                 ../../../../$4
+cp -f  ../Makefile*                        ../../../../$4
+cp -f  ../VGLite.def                       ../../../../$4
+cp -f  ./patch/0x425/VGLite*               ../../../../$4
+cp -f  ./patch/0x425/gc265_vs2019_425.bat  ../../../../$4
+cp -rf ../inc/                             ../../../../$4
+cp -rf ../VGLite/                          ../../../../$4
+cp -rf ../VGLiteKernel/                    ../../../../$4
+cp -rf ../../../../$3/$3/*                 ../../../../$4/BUILD_vgHuBi
 
-rm -rf ./package/VGLite/Series
-rm -rf ./package/VGLiteKernel/win32
 
-rm -f ./package/*.sh
-cp -f  release/build_linux.sh        ./package
+rm -rf ../../../../$4/VGLite/Series
+rm -rf ../../../../$4/VGLiteKernel/win32
+
+rm -f  ../../../../$4/*.sh
+rm -rf ../../../../$4/patch
+cp -f  ../release/build_linux.sh            ../../../../$4
+
 
 
