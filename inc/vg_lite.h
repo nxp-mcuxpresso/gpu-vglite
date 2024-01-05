60d4c580ba57a147e66b58fca4b2c31b76c34cb5
IPD_SCM_CN <IPD_SCM_CN@verisilicon.com>
2024-01-05 09:00:25 +0800

    [CL806311] Check in the missing changed file to fix the build errors.

--- inc/vg_lite.h
+++ inc/vg_lite.h
@@ -843,6 +843,7 @@ typedef unsigned int        vg_lite_color_t;
         vg_lite_uint8_t fc_enable;              /*! enable im fastclear. */
         vg_lite_uint8_t scissor_buffer;         /*! The buffer is scissor mask buffer. */
         vg_lite_uint8_t premultiplied;          /*! The RGB pixel values are alpha-premultipled */
+        vg_lite_uint8_t apply_premult;          /*! Need to apply alpha-premultiply */
     } vg_lite_buffer_t;
 
     /* Memory allocation info by kernel. */
