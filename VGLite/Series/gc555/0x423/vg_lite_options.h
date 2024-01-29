16d3a32b6c52cd45948dffc0fd772ad4dd5ef42c
IPD_SCM_CN <IPD_SCM_CN@verisilicon.com>
2024-01-29 17:34:44 +0800

    [CL827705] Added gcFEATURE_VG_SINGLE_COMMAND_BUFFER

--- VGLite/Series/gc555/0x423/vg_lite_options.h
+++ VGLite/Series/gc555/0x423/vg_lite_options.h
@@ -92,5 +92,6 @@
 #define gcFEATURE_VG_TRACE_API    0
 #define gcFEATURE_VG_POWER_MANAGEMENT           1
 #define gcFEATURE_VG_TILED_MODE    1
+#define gcFEATURE_VG_SINGLE_COMMAND_BUFFER   0
 
 #endif
