04c0c35f92fb3070cfc9bbf8257422f7d178d5af
IPD_SCM_CN <IPD_SCM_CN@verisilicon.com>
2024-03-21 06:22:56 +0800

    [CL875245] Add license header for dumpAPI files.

--- VGLite/dumpAPI.c
+++ VGLite/dumpAPI.c
@@ -1,4 +1,30 @@
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+/****************************************************************************
+*
+*    Copyright 2012 - 2023 Vivante Corporation, Santa Clara, California.
+*    All Rights Reserved.
+*
+*    Permission is hereby granted, free of charge, to any person obtaining
+*    a copy of this software and associated documentation files (the
+*    'Software'), to deal in the Software without restriction, including
+*    without limitation the rights to use, copy, modify, merge, publish,
+*    distribute, sub license, and/or sell copies of the Software, and to
+*    permit persons to whom the Software is furnished to do so, subject
+*    to the following conditions:
+*
+*    The above copyright notice and this permission notice (including the
+*    next paragraph) shall be included in all copies or substantial
+*    portions of the Software.
+*
+*    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+*    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+*    IN NO EVENT SHALL VIVANTE AND/OR ITS SUPPLIERS BE LIABLE FOR ANY
+*    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+*    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+*    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
@@ -9,6 +35,7 @@
 #else
 #include<stdalign.h>
 #endif //  _MSC_VER
+
 #define ALIGN(size, alignto) (((size) + (alignto) - 1) & ~((alignto) - 1))
 #define ALIGN_TO(size, alignto)\
     do{\
@@ -19,6 +46,10 @@
 #define RESOLUTION_2K_WIDTH 2048
 #define RESOLUTION_2K_HEIGHT 1024
 #define BYTE_PER_PIXEL 4
+
+
+#if DUMP_API
+
 int createVgl = 1;
 
 static int lgradEX_count = 0;
@@ -1269,3 +1300,5 @@ DLLEXPORT void FUNC_DUMP(vg_lite_set_command_buffer_size)(
 DLLEXPORT void FUNC_DUMP(vg_lite_init_grad)(vg_lite_linear_gradient_t* grad) {
     DUMP_FUNCTION(vg_lite_init_grad);
 }
+
+#endif /* DUMP_API */
