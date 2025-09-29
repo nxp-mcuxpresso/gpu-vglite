4a194c097f03d8efbbe548664777c931a1f897de
Harikrushna Parmar <harikrushna.parmar@nxp.com>
2025-09-29 12:24:05 +0530

    MGG-1698 [#ccc] vendor/nxp: Add middleware_vglite CMake component for RT1170
    Introduce `middleware_vglite_MIMXRT1176_cm7.cmake` under
    `vendor/nxp/rt1170_zephyr/` to define the VGLite build sources and
    include paths for the RT1170 Zephyr integration.
    
    The CMake component:
    - Adds core VGLite and kernel sources to `${MCUX_SDK_PROJECT_NAME}`.
    - Includes Zephyr-specific HAL and OS glue layers.
    - Configures required include directories for VGLite, and kernel headers.
    
    Signed-off-by: Harikrushna Parmar <harikrushna.parmar@nxp.com>

---
+++ vendor/nxp/rt1170_zephyr/middleware_vglite_MIMXRT1176_cm7.cmake
@@ -0,0 +1,23 @@
+include_guard()
+message("vglite-src component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${VgliteDir}/VGLite/vg_lite.c
+    ${VgliteDir}/VGLite/vg_lite_image.c
+    ${VgliteDir}/VGLite/vg_lite_matrix.c
+    ${VgliteDir}/VGLite/vg_lite_path.c
+    ${VgliteDir}/VGLite/zephyr/vg_lite_os.c
+    ${VgliteDir}/VGLite/vg_lite_stroke.c
+    ${VgliteDir}/VGLiteKernel/vg_lite_kernel.c
+    ${VgliteDir}/VGLiteKernel/zephyr/vg_lite_hal.c
+)
+
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    ${VgliteDir}/inc
+    ${VgliteDir}/VGLite/zephyr
+    ${VgliteDir}/VGLiteKernel
+    ${VgliteDir}/VGLiteKernel/zephyr    
+)
+
+
